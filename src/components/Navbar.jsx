4de8dc7f53e69b344a699b56a4664a8dc3eb2eb6
donvine mugendi <donvinemugendi@gmail.com>
2023-05-11 03:20:26 +0300

    my react app

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+function Navbar(){
+    return(
+        <header className="header">
+        <nav>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+            <li>Blog</li>
+            <li>Shop Now</li>
+          </ul>
+        </nav>
+      </header>
+    )
+}
+export default Navbar
